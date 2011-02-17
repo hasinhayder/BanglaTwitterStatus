adc71c365a3ff51f6496208cd12af5dd527bee62
hasinhayder <ironhide@leevio.com>
2011-02-17 17:39:58 +0600

    Schema Changed, added auto increment primary index for Posts table

--- schema/db.sql
+++ schema/db.sql
@@ -7,14 +7,13 @@ CREATE TABLE IF NOT EXISTS `posts` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 
-CREATE TABLE IF NOT EXISTS `users` (
+
+CREATE TABLE IF NOT EXISTS `posts` (
   `id` int(11) NOT NULL auto_increment,
-  `name` varchar(120) NOT NULL,
-  `token` varchar(200) NOT NULL,
-  `secret` varchar(200) NOT NULL,
-  `screen_name` varchar(200) NOT NULL,
-  `screen_id` bigint(20) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `status` text NOT NULL,
+  `short_url` varchar(200) default NULL,
+  `created` int(11) NOT NULL,
   PRIMARY KEY  (`id`),
-  UNIQUE KEY `screen_id` (`screen_id`),
-  KEY `name` (`name`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 ;
+  KEY `user_id` (`user_id`,`created`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
